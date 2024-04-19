eb128925d7cef24e5ea63f1880e6a06f137dcee8
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-04-19 12:12:06 -0500

    Update

---
+++ db/index.js
@@ -0,0 +1,9 @@
+// 1) Imports 
+require('dotenv').config();
+const pg = require('pg');
+
+// 2) Establish a client/DB connection
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost:5432/lumen');
+
+// 3) Export 
+module.exports = { client };
