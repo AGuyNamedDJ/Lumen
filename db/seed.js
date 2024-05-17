e635bc28a62baa4485b29df5d1937f88bc24c645
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-05-17 12:45:08 -0500

    Add initial strategies creation and test suite for strategy helper functions, fix unique constraint issue

--- db/seed.js
+++ db/seed.js
@@ -4,29 +4,32 @@ const { client } = require('./index');
 // File Imports
 const importSpecificCSVFile = require('./importHistoricalData');
 const { createUser, getAllUsers, getUserById, getUserByUsername, deleteUser, updateUser, loginUser } = require('./helperFunctions/user');
+const { createStrategy, getAllStrategies, getStrategyByName, getStrategyById, updateStrategy, deleteStrategy } = require('./helperFunctions/strategies');
+
+
 
 // Methods: Drop Tables
 async function dropTables(){
     try {
         console.log("Dropping tables... ");
         await client.query(`
-        DROP TABLE IF EXISTS users CASCADE;
-        DROP TABLE IF EXISTS strategies CASCADE;
-        DROP TABLE IF EXISTS trades CASCADE;
-        DROP TABLE IF EXISTS decision_rules;
-        DROP TABLE IF EXISTS alerts CASCADE;
-        DROP TABLE IF EXISTS market_data CASCADE;
-        DROP TABLE IF EXISTS audit_logs CASCADE;
-        DROP TABLE IF EXISTS configurations CASCADE;
-        DROP TABLE IF EXISTS historical_spx CASCADE;
-        DROP TABLE IF EXISTS real_time_spx CASCADE;
-    `);
+            DROP TABLE IF EXISTS users CASCADE;
+            DROP TABLE IF EXISTS strategies CASCADE;
+            DROP TABLE IF EXISTS trades CASCADE;
+            DROP TABLE IF EXISTS decision_rules;
+            DROP TABLE IF EXISTS alerts CASCADE;
+            DROP TABLE IF EXISTS market_data CASCADE;
+            DROP TABLE IF EXISTS audit_logs CASCADE;
+            DROP TABLE IF EXISTS configurations CASCADE;
+            DROP TABLE IF EXISTS historical_spx CASCADE;
+            DROP TABLE IF EXISTS real_time_spx CASCADE;
+        `);
         console.log("Finished dropping tables.")
     } catch(error){
         console.log("Error dropping tables!")
         console.log(error)
     }
-}
+};
 
 async function createTables() {
     try {
@@ -148,6 +151,36 @@ async function createInitialUsers() {
     }
 };
 
+// createInitialStrategies
+async function createInitialStrategies() {
+    console.log("Creating initial strategies...");
+    try {
+        const strategies = [
+            { name: 'Buy Stock', description: 'Buying the stock.' },
+            { name: 'Sell Stock', description: 'Selling the stock.' },
+            { name: 'Buy Call', description: 'Buying a call option.' },
+            { name: 'Sell Call', description: 'Selling a call option.' },
+            { name: 'Buy Put', description: 'Buying a put option.' },
+            { name: 'Sell Put', description: 'Selling a put option.' },
+            { name: 'Bull Call Spread', description: 'A vertical spread strategy where you buy a call and sell a higher strike call.' },
+            { name: 'Bear Put Spread', description: 'A vertical spread strategy where you buy a put and sell a lower strike put.' },
+            { name: 'Iron Condor', description: 'An options trading strategy that involves buying and selling four options contracts with different strike prices.' },
+        ];
+
+        for (const strategy of strategies) {
+            const existingStrategy = await getStrategyByName(strategy.name);
+            if (!existingStrategy) {
+                await createStrategy(strategy);
+            }
+        }
+        console.log("Finished creating initial strategies.");
+    } catch (error) {
+        console.error("Error creating initial strategies!");
+        console.error(error);
+    }
+};
+
+
 // Rebuild DB
 async function rebuildDB() {
     try {
@@ -155,6 +188,7 @@ async function rebuildDB() {
         await dropTables();
         await createTables();
         await createInitialUsers();
+        await createInitialStrategies();
         console.log('Tables have been successfully created.');
     } catch (error) {
         console.error("Error during rebuildDB!");
@@ -167,33 +201,60 @@ async function testDB() {
     try {
         console.log("Starting to test database...");
 
-        // Test User Helper FNs
-        const initialUser = await getUserByUsername('Admin');
-        console.log("Initial user", initialUser);
+        // // Test User Helper FNs
+        // console.log("Starting to test users...");
+        // const initialUser = await getUserByUsername('Admin');
+        // console.log("Initial user", initialUser);
 
-        if (initialUser) {
-            // Test getAllUsers
-            console.log("Calling getAllUsers...");
-            const allUsers = await getAllUsers();
-            console.log("All users", allUsers);
+        // if (initialUser) {
+        //     // Test getAllUsers
+        //     console.log("Calling getAllUsers...");
+        //     const allUsers = await getAllUsers();
+        //     console.log("All users", allUsers);
 
-            // Test getUserById
-            console.log("Calling getUserById for the initial user...");
-            const userById = await getUserById(initialUser.id);
-            console.log("User by ID", userById);
+        //     // Test getUserById
+        //     console.log("Calling getUserById for the initial user...");
+        //     const userById = await getUserById(initialUser.id);
+        //     console.log("User by ID", userById);
 
-            // Test updateUser
-            console.log("Updating initial user's last name...");
-            const updatedUser = await updateUser(initialUser.username, { last_name: 'UpdatedLastName' });
-            console.log("Updated user", updatedUser);
+        //     // Test updateUser
+        //     console.log("Updating initial user's last name...");
+        //     const updatedUser = await updateUser(initialUser.username, { last_name: 'UpdatedLastName' });
+        //     console.log("Updated user", updatedUser);
 
-            // Test deleteUser
-            console.log("Deleting the initial user...");
-            const deletedUser = await deleteUser(initialUser.username);
-            console.log("Deleted user", deletedUser);
-        };
+        //     // Test deleteUser
+        //     console.log("Deleting the initial user...");
+        //     const deletedUser = await deleteUser(initialUser.username);
+        //     console.log("Deleted user", deletedUser);
+        // };
 
-    // Test
+        // Test Strategies Helper FNs
+        console.log("Starting to test strategies...");
+        await createInitialStrategies();
+
+        // Get all strategies
+        console.log("Calling getAllStrategies...");
+        const allStrategies = await getAllStrategies();
+        console.log("All strategies", allStrategies);
+
+        // Assuming at least one strategy is created successfully
+        if (allStrategies.length > 0) {
+            // Get strategy by ID
+            console.log("Calling getStrategyById for the first strategy...");
+            const strategyById = await getStrategyById(allStrategies[0].id);
+            console.log("Strategy by ID", strategyById);
+
+            // Update strategy
+            console.log("Updating first strategy's description...");
+            const updatedStrategy = await updateStrategy(allStrategies[0].id, { description: 'Updated strategy description' });
+            console.log("Updated strategy", updatedStrategy);
+
+            // Delete strategy
+            console.log("Deleting the first strategy...");
+            const deletedStrategy = await deleteStrategy(allStrategies[0].id);
+            console.log("Deleted strategy", deletedStrategy);
+        }
+
 
     } catch (error) {
         console.log("Error during testDB!");
