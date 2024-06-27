1b87a52579d6e61c46c7b225e9f4de66f5c7120d
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-06-27 15:39:27 -0500

    complete SPX history added, imported, and functional 1978-Live

--- db/fetchS3Data.js
+++ db/fetchS3Data.js
@@ -13,12 +13,20 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 /**
- * Function to import data from a specific CSV file stored in an S3 bucket
+ * Function to remove commas from a string
+ * @param {string} str - The string to remove commas from
+ * @returns {string} - The string without commas
  */
-async function importSpecificCSVFile() {
+const removeCommas = str => str ? str.replace(/,/g, '') : '0';
+
+/**
+ * Function to import data from a specific CSV file stored in an S3 bucket
+ * @param {string} fileKey - The key of the CSV file in the S3 bucket
+ */
+async function importSpecificCSVFile(fileKey) {
     const params = {
         Bucket: 'spx-data-bucket', // Name of the S3 bucket
-        Key: 'HistoricalData_1713558856481.csv' // Key of the CSV file in the S3 bucket
+        Key: fileKey // Key of the CSV file in the S3 bucket
     };
 
     const stream = s3.getObject(params).createReadStream();
@@ -29,17 +37,33 @@ async function importSpecificCSVFile() {
     // Parse the CSV file
     stream.pipe(csv.parse({ headers: true }))
         .on('data', row => {
-            const [month, day, year] = row['Date'].split('/');
-            const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
-            records.push([
-                formattedDate,
-                parseFloat(row['Open']),
-                parseFloat(row['High']),
-                parseFloat(row['Low']),
-                parseFloat(row['Close/Last']),
-                parseInt(row['Volume'], 10) || null
-            ]);
+            let record;
+            // Check for the column names to handle different formats
+            if (row['Close/Last']) {
+                const [month, day, year] = row['Date'].split('/');
+                const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+                record = [
+                    formattedDate,
+                    parseFloat(removeCommas(row['Open'])),
+                    parseFloat(removeCommas(row['High'])),
+                    parseFloat(removeCommas(row['Low'])),
+                    parseFloat(removeCommas(row['Close/Last'])),
+                    parseInt(removeCommas(row['Volume']), 10) || null
+                ];
+            } else {
+                const [month, day, year] = row['Date'].split('/');
+                const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+                record = [
+                    formattedDate,
+                    parseFloat(removeCommas(row['Open'])),
+                    parseFloat(removeCommas(row['High'])),
+                    parseFloat(removeCommas(row['Low'])),
+                    parseFloat(removeCommas(row['Close'])),
+                    parseInt(removeCommas(row['Volume']), 10) || null
+                ];
+            }
 
+            records.push(record);
             totalRecords++;
             if (records.length >= 1000) {
                 insertRecords([...records], totalRecords, totalInserted);
@@ -51,7 +75,7 @@ async function importSpecificCSVFile() {
             if (records.length > 0) {
                 await insertRecords(records, totalRecords, totalInserted);
             }
-            console.log(`Finished importing data from S3. Total records processed: ${totalRecords}.`);
+            console.log(`Finished importing data from ${fileKey}. Total records processed: ${totalRecords}.`);
         })
         .on('error', error => {
             console.error('Error reading the CSV file:', error);
@@ -75,6 +99,7 @@ async function insertRecords(records, totalRecords, totalInserted) {
             await client.query(query, record);
         }
         await client.query('COMMIT');
+        totalInserted += records.length;
         const totalRemaining = totalRecords - totalInserted;
         console.log(`Inserted ${records.length} records successfully. Total records remaining: ${totalRemaining}`);
     } catch (error) {
@@ -83,4 +108,57 @@ async function insertRecords(records, totalRecords, totalInserted) {
     }
 }
 
-module.exports = importSpecificCSVFile;
+/**
+ * Function to import all CSV files from S3
+ */
+async function importAllCSVFiles() {
+    const files = [
+        'HistoricalData_1713558856481.csv',
+        'SPX-Data-2024.csv', 
+        'SPX-Data-2013.csv', 
+        'SPX-Data-2012.csv',
+        'SPX-Data-2011.csv', 
+        'SPX-Data-2010.csv', 
+        'SPX-Data-2009.csv',
+        'SPX-Data-2008.csv', 
+        'SPX-Data-2007.csv', 
+        'SPX-Data-2006.csv',
+        'SPX-Data-2005.csv', 
+        'SPX-Data-2004.csv',
+        'SPX-Data-2003.csv',
+        'SPX-Data-2002.csv',
+        'SPX-Data-2001.csv',
+        'SPX-Data-2000.csv',
+        'SPX-Data-1999.csv',
+        'SPX-Data-1998.csv',
+        'SPX-Data-1997.csv',
+        'SPX-Data-1996.csv',
+        'SPX-Data-1995.csv',
+        'SPX-Data-1994.csv',
+        'SPX-Data-1993.csv',
+        'SPX-Data-1992.csv',
+        'SPX-Data-1991.csv',
+        'SPX-Data-1990.csv',
+        'SPX-Data-1989.csv',
+        'SPX-Data-1988.csv',
+        'SPX-Data-1987.csv',
+        'SPX-Data-1986.csv',
+        'SPX-Data-1985.csv',
+        'SPX-Data-1984.csv',
+        'SPX-Data-1983.csv',
+        'SPX-Data-1982.csv',
+        'SPX-Data-1981.csv',
+        'SPX-Data-1980.csv',
+        'SPX-Data-1979.csv',
+        'SPX-Data-1978.csv'
+    ];
+
+    for (const file of files) {
+        console.log(`Starting to import data from ${file}`);
+        await importSpecificCSVFile(file);
+    }
+
+    console.log('Finished importing all CSV files.');
+}
+
+module.exports = importAllCSVFiles;
