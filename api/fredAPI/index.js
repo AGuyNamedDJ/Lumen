a0b6669ca5fc893fdc5d3708dc9fcc65cd6803da
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-08-07 17:49:01 -0500

    v2.0.7: Added CRUD helper functions and API endpoints for PPI data, including testing with Thunder Client.

--- api/fredAPI/index.js
+++ api/fredAPI/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fredAPIRouter = express.Router();
 
 const cpiRouter = require('./fredCPIData');
+const ppiRouter = require('./ppiData');
 fredAPIRouter.use('/cpi', cpiRouter);
+fredAPIRouter.use('/ppi', ppiRouter);
 
 module.exports = fredAPIRouter;
